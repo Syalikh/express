ed04d77ddd2cfc7d2528bf61a748548dc7f668e8
syalikh <syalikhfadin@gmail.com>
2023-05-28 15:07:09 +0700

    failed mysql

--- app/product/routes.js
+++ app/product/routes.js
@@ -3,25 +3,36 @@ const multer = require('multer');
 const upload = multer({dest: 'uploads'});
 const fs = require('fs');
 const path = require('path');
+const connection = require('../../config/mysql');
+const { error } = require('console');
 
-router.get('/', (req, res) => {
-    const {page, total} = req.query;
-    res.send({
-        status: 'Succesfully',
-        message: 'Welcome to Express JS Tutorial',
-        page,
-        total
-    });
+router.get('/barang', (req, res) => {
+    connection.connect();
+    connection.query({
+        sql: 'SELECT * FROM barang',
+    }, (error, result) => {
+        if(error) {
+            res.send({
+                status: 'failed',
+                response: 'failed to fetch data'
+            });
+        }else {
+            res.send({
+                status: 'success',
+                response: result
+            });
+        }
+    })
 });
 
-router.get('/product/:id', (req, res) => {
+router.get('/barang/:id', (req, res) => {
     res.json({
         id: req.params.id
     });
 });
 
-router.post('/product/', upload.single('image'), (req, res) => {
-    const {name, price, stock, status,} = req.body;
+router.post('/barang/', upload.single('image'), (req, res) => {
+    const {id, nama, harga, jumlah} = req.body;
     const image = req.file;
     if(image) {
         const target = path.join(__dirname, 'uploads', image.originalname);
